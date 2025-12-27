8fb9c8afeee01e8d0860ce3080a44a8d1609c605
Samuel <adeoyesamuel100@gmail.com>
2025-12-27 05:51:29 +0100

    project set-up

--- messaging_app/messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/messaging_app/settings.py
@@ -41,14 +41,18 @@ INSTALLED_APPS = [
 ]
 
 # Rest Framework Configuration
-REST_FRAMEWORk = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ],
+REST_FRAMEWORK = {
+  'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.IsAuthenticated',
+  ],
+  'DEFAULT_AUTHENTICATION_CLASSES': (
+      'rest_framework_simplejwt.authentication.JWTAuthentication',
+      'rest_framework.authentication.BasicAuthentication',
+      'rest_framework.authentication.SessionAuthentication'
+  ),
+  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+  'PAGE_SIZE': 20,
+  'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 MIDDLEWARE = [
